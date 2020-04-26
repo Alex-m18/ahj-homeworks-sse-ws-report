0f745ee591096e7d07485cbe4e8352c568b4f0e6
Aleksandr Melnikov <melnikov@tikhvin.org>
2020-04-26 16:51:38 +0300

    initial commit

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import OnlineReportWidget from './OnlineReportWidget';
-
-const url = 'https://alex-m18-ahj-sse-report.herokuapp.com/sse';
-
-const report = new OnlineReportWidget(url);
-report.bindToDOM(document.querySelector('#online_report_widget_container'));
-report.init();
